6d091831764298a77a7baf56a6157c9735a4aab8
SujanAroraSLK <arorasujaan@gmail.com>
2025-01-17 16:56:24 +0530

    fixed search by category

--- src/app/components/customer/navbar/navbar-content/navbarMenu.ts
+++ src/app/components/customer/navbar/navbar-content/navbarMenu.ts
@@ -1,93 +1,90 @@
 export const navigation = {
- 
-    women:{
-      id: 'women',
-      name: 'Women',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '/',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Basic Tees',
-          href: '/',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-      ],
-      sections: [
-        {
-          id: 'jewellerytype',
-          name: 'Jewellery Type',
-          items: [
-            { name: 'Earrings', id:"earrings", href: `{women/jewellerytype/earrings}` },
-            { name: 'Bangles', id:"bangles", href: '#' },
-            { name: 'Necklace', id: 'necklace' },
-            { name: 'Nose Pin', id: 'nosepin' },
-            { name: 'Bracelet', id: 'bracelet' },
-            { name: 'Ring', id: 'ring' },
-            { name: 'Pendant', id: 'pendant' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Tanishq', id: '#' },
-            { name: 'Giva', id: '#' },
-            { name: 'Senco', id: '#' },
-            { name: 'Carat Lane', id: '#' },
-          ],
-        },
-      ],
-    },
-    men:{
-      id: 'men',
-      name: 'Men',
-      featured: [
-        {
-          name: 'New Arrivals',
-          id: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        },
-        {
-          name: 'Artwork Tees',
-          id: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-          imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-        },
-      ],
-      sections: [
-        {
-          id: 'jewellerytype',
-          name: 'Jewellery Type',
-          items: [
-            { name: 'Chains', id: 'chains' },
-            { name: 'Bracelet', id: 'bracelet' },
-            { name: 'Ring', id: 'ring' },
-            { name: 'Stud', id: '#' },
-            { name: 'Kada', id: 'kada' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Tanishq', id: '#' },
-            { name: 'Giva', id: '#' },
-            { name: 'Senco', id: '#' },
-            { name: 'Carat Lane', id: '#' },
-          ],
-        },
-      ],
-    },
-
-  
-}
+  women: {
+    id: 'women',
+    name: 'Women',
+    featured: [
+      {
+        name: 'New Arrivals',
+        href: '/women/new-arrivals',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+        imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+      },
+      {
+        name: 'Basic Tees',
+        href: '/women/basic-tees',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+        imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+      },
+    ],
+    sections: [
+      {
+        id: 'jewellery',
+        name: 'Jewellery Type',
+        items: [
+          { name: 'Earrings', id: 'earrings', href: '/women/jewellery/earrings' },
+          { name: 'Bangles', id: 'bangles', href: '/women/jewellery/bangles' },
+          { name: 'Necklace', id: 'necklace', href: '/women/jewellery/necklace' },
+          { name: 'Nose Pin', id: 'nosepin', href: '/women/jewellery/nosepin' },
+          { name: 'Bracelet', id: 'bracelet', href: '/women/jewellery/bracelet' },
+          { name: 'Ring', id: 'ring', href: '/women/jewellery/ring' },
+          { name: 'Pendant', id: 'pendant', href: '/women/jewellery/pendant' },
+        ],
+      },
+      {
+        id: 'brands',
+        name: 'Brands',
+        items: [
+          { name: 'Tanishq', id: 'tanishq', href: '/women/brand/tanishq' },
+          { name: 'Giva', id: 'giva', href: '/women/brand/giva' },
+          { name: 'Senco', id: 'senco', href: '/women/brand/senco' },
+          { name: 'Carat Lane', id: 'caratlane', href: '/women/brand/caratlane' },
+        ],
+      },
+    ],
+  },
+  men: {
+    id: 'men',
+    name: 'Men',
+    featured: [
+      {
+        name: 'New Arrivals',
+        href: '/men/new-arrivals',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+        imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+      },
+      {
+        name: 'Artwork Tees',
+        href: '/men/artwork-tees',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+        imageAlt:
+          'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+      },
+    ],
+    sections: [
+      {
+        id: 'jewellery',
+        name: 'Jewellery Type',
+        items: [
+          { name: 'Chains', id: 'chains', href: '/men/jewellery/chains' },
+          { name: 'Bracelet', id: 'bracelet', href: '/men/jewellery/bracelet' },
+          { name: 'Ring', id: 'ring', href: '/men/jewellery/ring' },
+          { name: 'Stud', id: 'stud', href: '/men/jewellery/stud' },
+          { name: 'Kada', id: 'kada', href: '/men/jewellery/kada' },
+        ],
+      },
+      {
+        id: 'brands',
+        name: 'Brands',
+        items: [
+          { name: 'Tanishq', id: 'tanishq', href: '/men/brand/tanishq' },
+          { name: 'Giva', id: 'giva', href: '/men/brand/giva' },
+          { name: 'Senco', id: 'senco', href: '/men/brand/senco' },
+          { name: 'Carat Lane', id: 'caratlane', href: '/men/brand/caratlane' },
+        ],
+      },
+    ],
+  },
+};
 
 const categories = [
   {
@@ -96,39 +93,39 @@ const categories = [
     featured: [
       {
         name: 'New Arrivals',
-        href: '/',
+        href: '/women/new-arrivals',
         imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
         imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
       },
       {
         name: 'Basic Tees',
-        href: '/',
+        href: '/women/basic-tees',
         imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
         imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
       },
     ],
     sections: [
       {
-        id: 'jewellerytype',
+        id: 'jewellery',
         name: 'Jewellery Type',
         items: [
-          { name: 'Earrings', id:"earrings", href: `{women/jewellerytype/earrings}` },
-            { name: 'Bangles', id:"bangles", href: '#' },
-            { name: 'Necklace', id: 'necklace' },
-            { name: 'Nose Pin', id: 'nosepin' },
-            { name: 'Bracelet', id: 'bracelet' },
-            { name: 'Ring', id: 'ring' },
-            { name: 'Pendant', id: 'pendant' },
+          { name: 'Earrings', id: 'earrings', href: '/women/jewellery/earrings' },
+          { name: 'Bangles', id: 'bangles', href: '/women/jewellery/bangles' },
+          { name: 'Necklace', id: 'necklace', href: '/women/jewellery/necklace' },
+          { name: 'Nose Pin', id: 'nosepin', href: '/women/jewellery/nosepin' },
+          { name: 'Bracelet', id: 'bracelet', href: '/women/jewellery/bracelet' },
+          { name: 'Ring', id: 'ring', href: '/women/jewellery/ring' },
+          { name: 'Pendant', id: 'pendant', href: '/women/jewellery/pendant' },
         ],
       },
       {
         id: 'brands',
         name: 'Brands',
         items: [
-          { name: 'Tanishq', id: '#' },
-            { name: 'Giva', id: '#' },
-            { name: 'Senco', id: '#' },
-            { name: 'Carat Lane', id: '#' },
+          { name: 'Tanishq', id: 'tanishq', href: '/women/brand/tanishq' },
+          { name: 'Giva', id: 'giva', href: '/women/brand/giva' },
+          { name: 'Senco', id: 'senco', href: '/women/brand/senco' },
+          { name: 'Carat Lane', id: 'caratlane', href: '/women/brand/caratlane' },
         ],
       },
     ],
@@ -139,13 +136,13 @@ const categories = [
     featured: [
       {
         name: 'New Arrivals',
-        id: '#',
+        href: '/men/new-arrivals',
         imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
         imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
       },
       {
         name: 'Artwork Tees',
-        id: '#',
+        href: '/men/artwork-tees',
         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
         imageAlt:
           'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
@@ -153,116 +150,27 @@ const categories = [
     ],
     sections: [
       {
-        id: 'jewellerytype',
+        id: 'jewellery',
         name: 'Jewellery Type',
         items: [
-            { name: 'Chains', id: 'chains' },
-            { name: 'Bracelet', id: 'bracelet' },
-            { name: 'Ring', id: 'ring' },
-            { name: 'Stud', id: '#' },
-            { name: 'Kada', id: 'kada' },
-          
+          { name: 'Chains', id: 'chains', href: '/men/jewellery/chains' },
+          { name: 'Bracelet', id: 'bracelet', href: '/men/jewellery/bracelet' },
+          { name: 'Ring', id: 'ring', href: '/men/jewellery/ring' },
+          { name: 'Stud', id: 'stud', href: '/men/jewellery/stud' },
+          { name: 'Kada', id: 'kada', href: '/men/jewellery/kada' },
         ],
       },
       {
         id: 'brands',
         name: 'Brands',
         items: [
-          { name: 'Tanishq', id: '#' },
-            { name: 'Giva', id: '#' },
-            { name: 'Senco', id: '#' },
-            { name: 'Carat Lane', id: '#' },
+          { name: 'Tanishq', id: 'tanishq', href: '/men/brand/tanishq' },
+          { name: 'Giva', id: 'giva', href: '/men/brand/giva' },
+          { name: 'Senco', id: 'senco', href: '/men/brand/senco' },
+          { name: 'Carat Lane', id: 'caratlane', href: '/men/brand/caratlane' },
         ],
       },
     ],
   },
-]
-
-
-export const womenCategory={
-  id: 'women',
-  name: 'Women',
-  featured: [
-    {
-      name: 'New Arrivals',
-      href: '/',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-      imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-    },
-    {
-      name: 'Basic Tees',
-      href: '/',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-      imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-    },
-  ],
-  sections: [
-    {
-      id: 'jewellerytype',
-      name: 'Jewellery Type',
-      items: [
-        { name: 'Earrings', id:"earrings", href: `{women/jewellerytype/earrings}` },
-            { name: 'Bangles', id:"bangles", href: '#' },
-            { name: 'Necklace', id: 'necklace' },
-            { name: 'Nose Pin', id: 'nosepin' },
-            { name: 'Bracelet', id: 'bracelet' },
-            { name: 'Ring', id: 'ring' },
-            { name: 'Pendant', id: 'pendant' },
-      ],
-    },
-    {
-      id: 'brands',
-      name: 'Brands',
-      items: [
-        { name: 'Tanishq', id: '#' },
-            { name: 'Giva', id: '#' },
-            { name: 'Senco', id: '#' },
-            { name: 'Carat Lane', id: '#' },
-      ],
-    },
-  ],
-}
-export const menCategory={
-  id: 'men',
-  name: 'Men',
-  featured: [
-    {
-      name: 'New Arrivals',
-      id: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-      imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-    },
-    {
-      name: 'Artwork Tees',
-      id: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-      imageAlt:
-        'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-    },
-  ],
-  sections: [
-    {
-      id: 'jewellerytype',
-      name: 'Jewellery Type',
-      items: [
-        { name: 'Chains', id: 'chains' },
-            { name: 'Bracelet', id: 'bracelet' },
-            { name: 'Ring', id: 'ring' },
-            { name: 'Stud', id: '#' },
-            { name: 'Kada', id: 'kada' },
-        
-      ],
-    },
-    {
-      id: 'brands',
-      name: 'Brands',
-      items: [
-        { name: 'Tanishq', id: '#' },
-            { name: 'Giva', id: '#' },
-            { name: 'Senco', id: '#' },
-            { name: 'Carat Lane', id: '#' },
-      ],
-    },
-  ],
-}
+];
 
